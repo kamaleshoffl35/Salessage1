19bd5b20e4a8993663437de14f480af95a5289ee
Kamalesh <babukamalesh2@gmail.com>
2026-01-29 16:54:02 +0530

    final

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+   baseURL: "https://vyoobam-bix-backend.onrender.com/api",
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
