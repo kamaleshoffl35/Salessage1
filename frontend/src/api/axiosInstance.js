e7d73d8a91177cba404ab143a01d0b9b9aa8319b
Vyoobam-Tech <info@vyoobam.com>
2026-01-29 11:57:03 +0530

    final

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://salessage.onrender.com/api",
+ baseURL: "https://salessage.onrender.com/api",
+//baseURL: "http://localhost:5000/api",
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
