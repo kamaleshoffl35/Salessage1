50dbf63152d285a107271ca6c024100f077c6372
Kamalesh <babukamalesh2@gmail.com>
2026-03-18 13:53:29 +0530

    get orders api change

--- Backend/routes/getOrdersRoutes.js
+++ Backend/routes/getOrdersRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/getOrdersController");
-const {protect} = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 
 // ✅ My Orders Route
 router.get(
-  "/orders/my-orders",
+  "/my-orders",
   protect,
   orderController.getMyOrders
 );
