670ba0f1179cba8f51985b89075de036294dd796
Kamalesh <babukamalesh2@gmail.com>
2026-03-18 13:14:20 +0530

    CORS Preflight options fix

--- Backend/server.js
+++ Backend/server.js
@@ -73,7 +73,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
+
 
 app.use(express.json());
 // mongoose.connect("mongodb://127.0.0.1:27017/inventory")
