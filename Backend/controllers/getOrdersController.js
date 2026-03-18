3ca99b1d332c2667bb6a5c7bf84d60651a8759d1
Kamalesh <babukamalesh2@gmail.com>
2026-03-18 13:40:18 +0530

    get orders api change

--- Backend/controllers/getOrdersController.js
+++ Backend/controllers/getOrdersController.js
@@ -140,6 +140,53 @@
 //   }
 // };
 
+// exports.getMyOrders = async (req, res) => {
+//   try {
+//     const user = req.user;
+
+//     if (!user) {
+//       return res.status(401).json({ message: "Unauthorized" });
+//     }
+
+//  const tenant = req.headers["x-tenant-id"];
+
+// if (!tenant) {
+//   return res.status(400).json({ message: "Tenant missing" });
+// }
+
+//     const orders = await Order.find({
+//       website: tenant
+//     }).sort({ createdAt: -1 });
+
+//     const formattedOrders = orders.map((order) => ({
+//       _id: order._id,
+//       orderNumber: order.internal_order_id,
+//       createdAt: order.createdAt,
+//       orderStatus: order.order_status || "pending",
+//       paymentMethod: order.payment_mode?.toLowerCase(),
+//       paymentStatus: order.payment_status,
+//       shippingAddress: order.customer_details,
+//       subtotal: Math.round(order.amount / 1.18),
+//       tax: Math.round(order.amount - order.amount / 1.18),
+//       totalAmount: order.amount,
+//       items: order.products.map((p) => ({
+//         productDetails: {
+//           title: p.productDetails?.title,
+//           image: p.productDetails?.image,
+//         },
+//         selectedSize: p.selectedSize,
+//         qty: p.qty,
+//         price: p.price,
+//       })),
+//     }));
+
+//     res.json(formattedOrders);
+
+//   } catch (error) {
+//     console.error("getMyOrders Error:", error);
+//     res.status(500).json({ message: "Failed to fetch orders" });
+//   }
+// };
 exports.getMyOrders = async (req, res) => {
   try {
     const user = req.user;
@@ -148,11 +195,11 @@ exports.getMyOrders = async (req, res) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
- const tenant = req.headers["x-tenant-id"];
+    const tenant = req.headers["x-tenant-id"];
 
-if (!tenant) {
-  return res.status(400).json({ message: "Tenant missing" });
-}
+    if (!tenant) {
+      return res.status(400).json({ message: "Tenant missing" });
+    }
 
     const orders = await Order.find({
       website: tenant
@@ -169,7 +216,8 @@ if (!tenant) {
       subtotal: Math.round(order.amount / 1.18),
       tax: Math.round(order.amount - order.amount / 1.18),
       totalAmount: order.amount,
-      items: order.products.map((p) => ({
+
+      items: (order.products || []).map((p) => ({
         productDetails: {
           title: p.productDetails?.title,
           image: p.productDetails?.image,
